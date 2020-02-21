c7a7f083170ee0b6de1b5f97c38e01b143a5b8ea
Adam Sulewski <adam.j.sulewski@gmail.com>
2020-02-20 22:34:40 -0500

    create move on frontend

--- client/src/components/moves.tsx
+++ client/src/components/moves.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { gql } from 'apollo-boost'
-import { useQuery } from '@apollo/react-hooks'
+import { useQuery, useMutation } from '@apollo/react-hooks'
 
 type MoveType = 'TURN' | 'STEP'
 
@@ -49,10 +49,97 @@ const MoveList = () => {
   )
 }
 
+const ADD_MOVE = gql`
+  mutation($type: MoveType!, $name: String!, $notes: String!) {
+    createMove(type: $type, name: $name, notes: $notes) {
+      id,
+      name,
+      type,
+      notes
+    }
+  }
+`
+
+const MoveForm = () => {
+  const [ name, setName ] = useState('')
+  const [ notes, setNotes ] = useState('')
+  const [ moveType, setMoveType ] = useState('STEP')
+
+  const [ addMove, _ ] = useMutation(
+    ADD_MOVE,
+    {
+      update(cache, { data: { createMove } }) {
+        const result = cache.readQuery<{moves: Move[]}>({ query: MOVE_LIST })
+        if (result) {
+          cache.writeQuery({
+            query: MOVE_LIST,
+            data: { moves: [...result.moves, createMove] }
+          })
+        }
+      }
+    }
+  )
+
+  const onSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault()
+
+    const variables = { name, notes, type: moveType }
+    addMove({ variables })
+
+    setName('')
+    setNotes('')
+    setMoveType('STEP')
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        value={name}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          const { value } = e.target
+          setName(value)
+        }}
+      />
+
+      <label htmlFor="notes">Notes</label>
+      <textarea
+        name="notes"
+        id="notes"
+        value={notes}
+        onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+          const { value } = e.target
+          setNotes(value)
+        }}
+      />
+
+      <label htmlFor="type">Type</label>
+      <select
+        name="type"
+        id="type"
+        value={moveType}
+        onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+          const { value } = e.target
+          setMoveType(value)
+        }}
+      >
+        <option value="STEP">Step</option>
+        <option value="TURN">Turn</option>
+      </select>
+
+      <button>Save</button>
+    </form>
+  )
+}
+
 const Moves = () => {
   return (
     <section className="moves">
       <h2>Moves</h2>
+      <MoveForm />
       <MoveList />
     </section>
   )
