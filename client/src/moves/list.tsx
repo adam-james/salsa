04199dae0f089983d48d31991b95be9559a5c323
Adam Sulewski <adam.j.sulewski@gmail.com>
2020-02-25 20:16:58 -0500

    add filter table

--- client/src/moves/list.tsx
+++ client/src/moves/list.tsx
@@ -1,14 +1,125 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, RouteComponentProps } from '@reach/router'
 import { Move } from './types'
 import { gql } from 'apollo-boost'
 import { useQuery } from '@apollo/react-hooks'
+import { Label, Option, Select } from '../components/form'
 
-const MoveListItem = ({ move }: { move: Move }) => {
+// COMPONENTS
+import styled from 'styled-components'
+
+const Table = styled.table`
+  width: 100%;
+  text-align: left;
+`
+
+const TableHead = styled.thead``
+
+const TableHeadRow = styled.tr``
+
+const TH = styled.th``
+
+const TableBody = styled.tbody``
+
+const TableBodyRow = styled.tr``
+
+const TD = styled.td``
+
+type MoveTableProps = {
+  moves: Move[]
+}
+
+const MoveTable = (props: MoveTableProps) => {
   return (
-    <li>{move.name} -- {move.type} -- {move.style}</li>
+    <Table>
+      <TableHead>
+        <TableHeadRow>
+          <TH>Name</TH>
+          <TH>Style</TH>
+          <TH>Type</TH>
+        </TableHeadRow>
+      </TableHead>
+      <TableBody>
+        {props.moves.map(move => {
+          return (
+            <TableBodyRow key={move.id}>
+              <TD>{move.name}</TD>
+              <TD>{move.style}</TD>
+              <TD>{move.type}</TD>
+            </TableBodyRow>
+          )
+        })}
+      </TableBody>
+    </Table>
   )
 }
+
+type MoveFilterTableProps = {
+  moves: Move[]
+}
+
+const MoveFilterTable = (props: MoveFilterTableProps) => {
+  const [ moveType, setMoveType ] = useState('')
+  const [ salsaStyle, setSalsaStyle ] = useState('')
+
+  const moves: Move[] = props.moves.filter(m => {
+    if (moveType && salsaStyle) {
+      return m.style === salsaStyle && m.type === moveType
+    }
+    
+    if (moveType) {
+      return m.type === moveType
+    }
+
+    if (salsaStyle) {
+      return m.style === salsaStyle
+    }
+
+    return true
+  })
+
+  return (
+    <>
+      <Label htmlFor="type">Type</Label>
+      <Select
+        name="type"
+        id="type"
+        value={moveType}
+        onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+          const { value } = e.target
+          setMoveType(value)
+        }}
+      >
+        <Option value=""></Option>
+        <Option value="STEP">Step</Option>
+        <Option value="TURN">Turn</Option>
+      </Select>
+
+      <Label htmlFor="style">Style</Label>
+      <Select
+        name="style"
+        id="style"
+        value={salsaStyle}
+        onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+          const { value } = e.target
+          setSalsaStyle(value)
+        }}
+      >
+        <Option value=""></Option>
+        <Option value="COLOMBIAN">Colombian</Option>
+        <Option value="CUBAN">Cuban</Option>
+        <Option value="ON_ONE">On One</Option>
+        <Option value="ON_TWO">On Two</Option>
+      </Select>
+
+      <MoveTable moves={moves} />
+    </>
+  )
+}
+
+const PageContainer = styled.div`
+  padding: 16px;
+`
 
 export const MOVE_LIST = gql`
   {
@@ -32,24 +143,20 @@ const MoveList = () => {
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error :(</p>
 
-  return (
-    <ul>
-      {data && data.moves.map((move: Move) => {
-        return (
-          <MoveListItem move={move} key={move.id} />
-        )
-      })}
-    </ul>
-  )
+  if (data) {
+    return <MoveFilterTable moves={data.moves} />
+  }
+
+  return null
 }
 
 const MovesList = (props: RouteComponentProps) => {
   return (
-    <>
+    <PageContainer>
       <h2>Moves</h2>
       <Link to="/moves/new">New Move</Link>
       <MoveList />
-    </>
+    </PageContainer>
   )
 }
 
